d9ab310d9890137280483ee232234bd977d33005
David faidley <david.faidley23@gmail.com>
2022-05-11 11:57:28 -0600

    added new snips and added img for files

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ function Header(props) {
         <header className="App-header">
             <img src={props.logo} className="App-logo" alt="logo" />
             <Nav className='App-link' github='https://github.com/dfaidley23/' linkedIn='https://www.linkedin.com/in/david-faidley-5208551a4/' about='#abount-me' projects='#my-projects' resume='#my-resume' contact='#contact'/>
+            <img src={props.logo} className="App-logo" alt="logo" />
         </header>
     )
 };
