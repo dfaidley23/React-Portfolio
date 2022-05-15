fd01147f277c3ef0f55bb100f096f476ab10cb44
David faidley <david.faidley23@gmail.com>
2022-05-15 15:08:54 -0600

    had to change some issues with deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {
   BrowserRouter as Router,
 } from "react-router-dom";
@@ -17,5 +16,3 @@ root.render(
     </Router>
   </React.StrictMode>
 );
-
-reportWebVitals();
